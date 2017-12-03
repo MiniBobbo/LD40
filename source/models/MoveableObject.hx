402b4ada326fb4eb1b5e41ea3d3132518eccd3fc
Dave Murtagh <davidmurtagh@hotmail.com>
2017-12-02 21:40:01 -0500

    Fixed random errors
    Enum strangeness.

--- source/models/MoveableObject.hx
+++ source/models/MoveableObject.hx
@@ -1,8 +1,9 @@
 package models;
+import models.GridEntity.TypeOfObject;
 
 class MoveableObject extends GridEntity {
 
-    public type:TypeOfObject = TypeOfObject.None;
+    public var type:TypeOfObject = TypeOfObject.None;
     public function new (type:TypeOfObject){
         super();
         this.type = type;
--- source/models/Person.hx
+++ source/models/Person.hx
@@ -1,4 +1,5 @@
 package models;
+import models.GridEntity.TypeOfObject;
 
 class Person extends GridEntity {
 
